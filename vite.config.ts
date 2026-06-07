31fe0bed03dac6329741d13929564816f92253f0
yekta <ngyekta@gmail.com>
2026-06-07 03:03:22 +0300

    Revert "Sync package json"
    This reverts commit 0258ed010d2c3adbca4633d1702879523a107460.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -12,11 +12,11 @@ export default defineConfig({
     tsconfigPaths: true,
   },
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     tanstackStart({
       srcDirectory: "src",
     }),
     viteReact(),
+    nitro(),
   ],
 });

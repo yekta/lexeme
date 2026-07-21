2ca1b0392856fa6df479ad39e064cfeb59ac7916
yekta <ngyekta@gmail.com>
2026-07-21 08:35:37 +0300

    Transform

--- src/components/suggest-button.tsx
+++ src/components/suggest-button.tsx
@@ -28,16 +28,14 @@ export function SuggestButton({
       onClick={onClick}
     >
       <div className="size-3.5 shrink-0 -ml-0.5">
-        <div
-          data-pending={isPending || undefined}
-          className="size-full data-pending:animate-spin transform-gpu"
-        >
-          {isPending ? (
-            <LoaderIcon className="size-full" />
-          ) : (
-            <Sparkles className="size-full" />
-          )}
-        </div>
+        {isPending ? (
+          <LoaderIcon
+            data-pending={isPending || undefined}
+            className="size-full data-pending:animate-spin transform-gpu"
+          />
+        ) : (
+          <Sparkles className="size-full" />
+        )}
       </div>
       <span className="truncate">{isPending ? "Suggesting" : "Suggest"}</span>
     </Button>
